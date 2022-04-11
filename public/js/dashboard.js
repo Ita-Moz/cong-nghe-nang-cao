fd4b206a22e7ed7ad6647dd163fe04deb1636125
manh <luongvanmanh1929@gmail.com>
2022-04-11 22:59:00 +0700

    lan3

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,38 @@
 
-$(document).ready(()=>{
-// 
-// 
+$(document).ready(() => {
+  // 
+  $('#btn_delete').on('click', () => {
+    let id = $('#btn_delete').attr("data-id");
+    // $.ajax({
+    //   url: 'http://localhost:3000/admin/deleted/'+ id,
+    //   type: 'DELETE',
+    //   success: function (kq) {
+    //     // res.redirect("http://localhost:3000/admin/dashboard");
+    //     console.log(kq)
+    //   },
+    //   error: function(){
+    //     console.log(error);
+    //   }
+    // });
+    $.ajax({
+      url: 'http://localhost:3000/admin/deleted/' + id,
+      type: 'DELETE'
+    });
+
+    success(function (kq) {
+      // res.redirect("http://localhost:3000/admin/dashboard");
+      console.log("chao" + kq)
+    })
+
+    error(function () {
+      console.log(error);
+    });
+  })
+  // 
   document.querySelector(".jsFilter").addEventListener("click", function () {
     document.querySelector(".filter-menu").classList.toggle("active");
   });
-  
+
   document.querySelector(".grid").addEventListener("click", function () {
     document.querySelector(".list").classList.remove("active");
     document.querySelector(".grid").classList.add("active");
@@ -14,18 +41,18 @@ $(document).ready(()=>{
       .querySelector(".products-area-wrapper")
       .classList.remove("tableView");
   });
-  
+
   document.querySelector(".list").addEventListener("click", function () {
     document.querySelector(".list").classList.add("active");
     document.querySelector(".grid").classList.remove("active");
     document.querySelector(".products-area-wrapper").classList.remove("gridView");
     document.querySelector(".products-area-wrapper").classList.add("tableView");
   });
-  
+
   var modeSwitch = document.querySelector(".mode-switch");
   modeSwitch.addEventListener("click", function () {
     document.documentElement.classList.toggle("light");
     modeSwitch.classList.toggle("active");
   });
-  
+
 })
